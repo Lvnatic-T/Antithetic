303ab4fecac4ae44814e94d021ff6b7c6447e0f8
Lvnatic <thomaslinnemann55@gmail.com>
2025-02-28 07:51:51 +0100

    Added right-click interaction to appliances

--- src/main/java/net/lvnatic/antithetic/block/custom/RotomFrost.java
+++ src/main/java/net/lvnatic/antithetic/block/custom/RotomFrost.java
@@ -5,12 +5,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class RotomFrost extends HorizontalFacingBlock {
     public static final MapCodec<RotomFrost> CODEC = createCodec(RotomFrost::new);
@@ -37,5 +49,18 @@ public class RotomFrost extends HorizontalFacingBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.antithetic.rotom.tooltip"));
+        super.appendTooltip(stack, context, tooltip, options);
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player,
+                                 BlockHitResult hit) {
+        world.playSound(player, pos, SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.BLOCKS, 1f, 1f);
+        return ActionResult.SUCCESS;
     }
 }
